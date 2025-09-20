9cbc497a29c8bcfb6511f8562927165309278ec0
BenjaminBoyle1 <boyle153@gmail.com>
2025-09-20 13:30:22 -0600

    tweaks

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 const port = process.env.PORT || 3000;
 app.use('/', require('./routes'));
 
-app.listen(port, function () {
+app.listen(port, () => {
     console.log('Server is running on port ${port}');
 });
